46d72beca3b1f8780bd51b54954fb5c98d04ba02
Leo Gao <54557097+leogao2@users.noreply.github.com>
2021-09-28 17:11:43 -0600

    Update init_ray_v2.sh

--- scripts/init_ray_v2.sh
+++ scripts/init_ray_v2.sh
@@ -9,13 +9,4 @@ screen -d -m python3 -c 'import time; time.sleep(999999999)'
 sudo pip install "jax[tpu]>=0.2.18" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 sudo pip install --upgrade ray[default]==1.5.1 fabric dataclasses optax git+https://github.com/deepmind/dm-haiku tqdm cloudpickle smart_open[gcs] einops func_timeout
 
-# sudo apt install curl gnupg
-# curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
-# echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-# sudo apt update && sudo apt install bazel
-
-# sudo git clone https://github.com/leogao2/ray || echo already cloned
-# cd ray
-# sudo git checkout no_ver
-# cd python
-# sudo python3 setup.py install
+curl https://raw.githubusercontent.com/leogao2/ray/4db2c807f34ba86de18a994ce51730cf24898fea/python/ray/_private/services.py | sudo tee /usr/local/lib/python3.8/dist-packages/ray/_private/services.py
