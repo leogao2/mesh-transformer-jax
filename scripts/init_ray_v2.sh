592f2a579df159fadfc4fb7a22b5e03e0500a2e2
Leo Gao <54557097+leogao2@users.noreply.github.com>
2021-09-28 15:53:10 -0600

    use no version detection version of ray

--- scripts/init_ray_v2.sh
+++ scripts/init_ray_v2.sh
@@ -7,3 +7,9 @@ screen -d -m python -c 'import time; time.sleep(999999999)'
 # initializes jax and installs ray on cloud TPUs
 sudo pip install "jax[tpu]>=0.2.18" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 sudo pip install --upgrade ray[default]==1.5.1 fabric dataclasses optax git+https://github.com/deepmind/dm-haiku tqdm cloudpickle smart_open[gcs] einops func_timeout
+
+git clone https://github.com/leogao2/ray
+cd ray
+git checkout no_ver
+cd python
+sudo python setup.py install
