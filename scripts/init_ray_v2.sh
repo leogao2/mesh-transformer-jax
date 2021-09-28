b56aa41d5360fd6f5042d621ebcf74083032fb1d
Leo Gao <54557097+leogao2@users.noreply.github.com>
2021-09-28 16:29:49 -0600

    Update init_ray_v2.sh

--- scripts/init_ray_v2.sh
+++ scripts/init_ray_v2.sh
@@ -3,6 +3,7 @@ set -e
 
 # this locks the python executable down to hopefully stop if from being fiddled with...
 screen -d -m python -c 'import time; time.sleep(999999999)'
+screen -d -m python3 -c 'import time; time.sleep(999999999)'
 
 # initializes jax and installs ray on cloud TPUs
 sudo pip install "jax[tpu]>=0.2.18" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
