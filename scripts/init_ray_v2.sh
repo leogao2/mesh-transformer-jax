ea79ba699d271ed5de7b67e5091674f93ea5ef0f
Leo Gao <54557097+leogao2@users.noreply.github.com>
2021-09-28 16:24:22 -0600

    Update init_ray_v2.sh

--- scripts/init_ray_v2.sh
+++ scripts/init_ray_v2.sh
@@ -8,7 +8,7 @@ screen -d -m python -c 'import time; time.sleep(999999999)'
 sudo pip install "jax[tpu]>=0.2.18" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 sudo pip install --upgrade ray[default]==1.5.1 fabric dataclasses optax git+https://github.com/deepmind/dm-haiku tqdm cloudpickle smart_open[gcs] einops func_timeout
 
-git clone https://github.com/leogao2/ray
+git clone https://github.com/leogao2/ray || echo already cloned
 cd ray
 git checkout no_ver
 cd python
